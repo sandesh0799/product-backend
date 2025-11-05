24980d8b3134bec0283cf9e2037e4221d4f4d42f
sandesh <sandesh.sawant@sparklesoft.co.in>
2025-11-05 10:27:11 +0530

    category cannot delete if product belong to it.

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -42,20 +42,18 @@ exports.updateCategory = async (req, res) => {
 
 // Delete Category
 exports.deleteCategory = async (req, res) => {
-    try {
-        const { id } = req.params;
-
-        const category = await Category.findByIdAndDelete(id);
-        if (!category) {
-            return res.status(404).json({ message: 'Category not found' });
-        }
-
-        res.status(200).json({ message: 'Category deleted successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
+    const { id } = req.params;
+    const category = await Category.findById(id);
+    if (!category) {
+        return res.status(404).json({ message: 'Category not found' });
     }
-};
+    const products = await Product.find({ category: id });
+    if (products.length > 0) {
+        return res.status(400).json({ message: 'Category cannot be deleted if products belong to it' });
+    }
+    await category.deleteOne();
+    res.status(200).json({ message: 'Category deleted successfully' });
+};   
 
 // Get All Categories
 exports.getCategories = async (req, res) => {
